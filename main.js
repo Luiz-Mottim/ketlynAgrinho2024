1c4fddd4c96c832a1c1f831ddcb42b51dc02ebcf
Prof Luiz <105397969+Luiz-Mottim@users.noreply.github.com>
2024-08-01 18:13:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('btn1').addEventListener('click', function() {
+    console.log('Ação 1 executada');
+});
+
+document.getElementById('btn2').addEventListener('click', function() {
+    console.log('Ação 2 executada');
+});
